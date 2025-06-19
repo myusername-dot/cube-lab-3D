44011a04fa9f0bb92fdf081c2c965f9a74ea1924
myusername-dot <andrej.iljuhin@ddgcorp.ru>
2025-06-19 14:56:04 +0300

    refactor Cell3D

--- core/src/main/java/io/github/labyrinthgenerator/pages/game3d/cell/Cell3D.java
+++ core/src/main/java/io/github/labyrinthgenerator/pages/game3d/cell/Cell3D.java
@@ -3,7 +3,9 @@ package io.github.labyrinthgenerator.pages.game3d.cell;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.Shader;
 import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute;
 import com.badlogic.gdx.math.Vector3;
 import io.github.labyrinthgenerator.pages.game3d.entities.Entity;
@@ -31,11 +33,6 @@ public class Cell3D extends Entity {
 	public boolean hasCeiling = false;
     public boolean mobSpawn = false;
 
-	public boolean neighbourCellNorth = false;
-	public boolean neighbourCellSouth = false;
-	public boolean neighbourCellWest = false;
-	public boolean neighbourCellEast = false;
-
 	public Texture texRegNorth, texRegSouth, texRegWest, texRegEast, texRegFloor, texRegCeiling;
 
 	public Cell3D(final Vector3 position, final GameScreen screen) {
@@ -43,122 +40,71 @@ public class Cell3D extends Entity {
 		this.position.set(position.cpy().add(0, HALF_UNIT, 0));
 	}
 
+    private ModelInstanceBB createModelInstanceBB(Model model, Texture texture) {
+        ModelInstanceBB modelInstanceBB = new ModelInstanceBB(model);
+        if (texture != null) {
+            final TextureAttribute ta = (TextureAttribute) modelInstanceBB.materials.get(0)
+                .get(TextureAttribute.Diffuse);
+            ta.set(new TextureRegion(texRegNorth));
+        }
+        return modelInstanceBB;
+    }
+
 	public void buildCell() {
 		if (hasWallNorth) {
-			mdlInstWallNorth = new ModelInstanceBB(screen.game.getCellBuilder().mdlWallNorth);
-
-			if (texRegNorth != null) {
-				final TextureAttribute ta = (TextureAttribute) mdlInstWallNorth.materials.get(0)
-						.get(TextureAttribute.Diffuse);
-				ta.set(new TextureRegion(texRegNorth));
-			}
-
+			mdlInstWallNorth = createModelInstanceBB(screen.game.getCellBuilder().mdlWallNorth, texRegNorth);
 			mdlInstWallNorth.transform.setToTranslation(this.position.cpy().add(new Vector3(0, 0, -HALF_UNIT)));
 		}
-
 		if (hasWallSouth) {
-			mdlInstWallSouth = new ModelInstanceBB(screen.game.getCellBuilder().mdlWallSouth);
-
-			if (texRegSouth != null) {
-				final TextureAttribute ta = (TextureAttribute) mdlInstWallSouth.materials.get(0)
-						.get(TextureAttribute.Diffuse);
-				ta.set(new TextureRegion(texRegSouth));
-			}
-
+			mdlInstWallSouth = createModelInstanceBB(screen.game.getCellBuilder().mdlWallSouth, texRegSouth);
 			mdlInstWallSouth.transform.setToTranslation(this.position.cpy().add(new Vector3(0, 0, HALF_UNIT)));
 		}
-
 		if (hasWallWest) {
-			mdlInstWallWest = new ModelInstanceBB(screen.game.getCellBuilder().mdlWallWest);
-
-			if (texRegWest != null) {
-				final TextureAttribute ta = (TextureAttribute) mdlInstWallWest.materials.get(0)
-						.get(TextureAttribute.Diffuse);
-				ta.set(new TextureRegion(texRegWest));
-			}
-
+			mdlInstWallWest = createModelInstanceBB(screen.game.getCellBuilder().mdlWallWest, texRegWest);
 			mdlInstWallWest.transform.setToTranslation(this.position.cpy().add(new Vector3(HALF_UNIT, 0, 0)));
 		}
-
 		if (hasWallEast) {
-			mdlInstWallEast = new ModelInstanceBB(screen.game.getCellBuilder().mdlWallEast);
-
-			if (texRegEast != null) {
-				final TextureAttribute ta = (TextureAttribute) mdlInstWallEast.materials.get(0)
-						.get(TextureAttribute.Diffuse);
-				ta.set(new TextureRegion(texRegEast));
-			}
-
+			mdlInstWallEast = createModelInstanceBB(screen.game.getCellBuilder().mdlWallEast, texRegEast);
 			mdlInstWallEast.transform.setToTranslation(this.position.cpy().add(new Vector3(-HALF_UNIT, 0, 0)));
 		}
-
 		if (hasFloor) {
-			mdlInstFloor = new ModelInstanceBB(screen.game.getCellBuilder().mdlFloor);
-
-			if (texRegFloor != null) {
-				final TextureAttribute ta = (TextureAttribute) mdlInstFloor.materials.get(0)
-						.get(TextureAttribute.Diffuse);
-				ta.set(new TextureRegion(texRegFloor));
-			}
-
+			mdlInstFloor = createModelInstanceBB(screen.game.getCellBuilder().mdlFloor, texRegFloor);
 			mdlInstFloor.transform.setToTranslation(this.position.cpy().add(new Vector3(0, HALF_UNIT, 0)));
 		}
-
 		if (hasCeiling) {
-			mdlInstCeiling = new ModelInstanceBB(screen.game.getCellBuilder().mdlCeiling);
-
-			if (texRegCeiling != null) {
-				final TextureAttribute ta = (TextureAttribute) mdlInstCeiling.materials.get(0)
-						.get(TextureAttribute.Diffuse);
-				ta.set(new TextureRegion(texRegCeiling));
-			}
-
+			mdlInstCeiling = createModelInstanceBB(screen.game.getCellBuilder().mdlCeiling, texRegCeiling);
 			mdlInstCeiling.transform.setToTranslation(this.position.cpy().add(new Vector3(0, -HALF_UNIT, 0)));
 		}
 	}
 
+    private void setInFrustum(final ModelInstanceBB model, final ModelBatch mdlBatch, final Environment env, final Shader shader) {
+        model.setInFrustum(screen.frustumCull(screen.getCurrentCam(), model));
+        if (model.isInFrustum()) {
+            mdlBatch.render(model, env, shader);
+        }
+    }
+
 	@Override
 	public void render3D(final ModelBatch mdlBatch, final Environment env, final float delta) {
+        Shader shader = ((MyShaderProvider) screen.game.getShaderProvider()).getShader();
+
 		if (hasWallNorth) {
-			mdlInstWallNorth.setInFrustum(screen.frustumCull(screen.getCurrentCam(), mdlInstWallNorth));
-			if (mdlInstWallNorth.isInFrustum()) {
-				mdlBatch.render(mdlInstWallNorth, env, ((MyShaderProvider) screen.game.getShaderProvider()).getShader());
-			}
+			setInFrustum(mdlInstWallNorth, mdlBatch, env, shader);
 		}
-
 		if (hasWallSouth) {
-			mdlInstWallSouth.setInFrustum(screen.frustumCull(screen.getCurrentCam(), mdlInstWallSouth));
-			if (mdlInstWallSouth.isInFrustum()) {
-				mdlBatch.render(mdlInstWallSouth, env, ((MyShaderProvider) screen.game.getShaderProvider()).getShader());
-			}
+            setInFrustum(mdlInstWallSouth, mdlBatch, env, shader);
 		}
-
 		if (hasWallWest) {
-			mdlInstWallWest.setInFrustum(screen.frustumCull(screen.getCurrentCam(), mdlInstWallWest));
-			if (mdlInstWallWest.isInFrustum()) {
-				mdlBatch.render(mdlInstWallWest, env, ((MyShaderProvider) screen.game.getShaderProvider()).getShader());
-			}
+            setInFrustum(mdlInstWallWest, mdlBatch, env, shader);
 		}
-
 		if (hasWallEast) {
-			mdlInstWallEast.setInFrustum(screen.frustumCull(screen.getCurrentCam(), mdlInstWallEast));
-			if (mdlInstWallEast.isInFrustum()) {
-				mdlBatch.render(mdlInstWallEast, env, ((MyShaderProvider) screen.game.getShaderProvider()).getShader());
-			}
+            setInFrustum(mdlInstWallEast, mdlBatch, env, shader);
 		}
-
 		if (hasFloor) {
-			mdlInstFloor.setInFrustum(screen.frustumCull(screen.getCurrentCam(), mdlInstFloor));
-			if (mdlInstFloor.isInFrustum()) {
-				mdlBatch.render(mdlInstFloor, env, ((MyShaderProvider) screen.game.getShaderProvider()).getShader());
-			}
+            setInFrustum(mdlInstFloor, mdlBatch, env, shader);
 		}
-
 		if (hasCeiling) {
-			mdlInstCeiling.setInFrustum(screen.frustumCull(screen.getCurrentCam(), mdlInstCeiling));
-			if (mdlInstCeiling.isInFrustum()) {
-				mdlBatch.render(mdlInstCeiling, env, ((MyShaderProvider) screen.game.getShaderProvider()).getShader());
-			}
+            setInFrustum(mdlInstCeiling, mdlBatch, env, shader);
 		}
 	}
 
