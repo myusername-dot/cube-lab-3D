b6b7a610fbacd4b7ca0d14be779bcf8db69894b6
myusername-dot <andrej.iljuhin@ddgcorp.ru>
2025-06-21 17:20:23 +0300

    refactor Vector2i

--- core/src/main/java/io/github/labyrinthgenerator/additional/Vector2iSeedHash.java
+++ core/src/main/java/io/github/labyrinthgenerator/additional/Vector2iSeedHash.java
@@ -1,38 +1,32 @@
 package io.github.labyrinthgenerator.additional;
 
+import io.github.labyrinthgenerator.pages.game3d.vectors.Vector2i;
+
 import java.util.Random;
 
-public class Vector2i {
+public class Vector2iSeedHash extends Vector2i {
     private static final Random random = new Random();
 
-    public int x;
-    public int y;
-
-    public Vector2i(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public Vector2i(Vector2i v) {
-        this.x = v.x;
-        this.y = v.y;
+    public Vector2iSeedHash(int x, int y) {
+        super(x, y);
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Vector2i vector2 = (Vector2i) o;
+        Vector2iSeedHash vector2 = (Vector2iSeedHash) o;
         return x == vector2.x && y == vector2.y;
     }
 
+    // only for stream limit
     @Override
     public int hashCode() {
         random.setSeed((long) x * y);
         return random.nextInt();
     }
 
-    public Double getDistance(Vector2i vector2) {
+    public Double getDistance(Vector2iSeedHash vector2) {
         int dx = vector2.x - x;
         int dy = vector2.y - y;
         return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
