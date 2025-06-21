bcf49d1b2c27becf13cf36b8ac8edcbe5da91300
myusername-dot <andrej.iljuhin@ddgcorp.ru>
2025-06-21 16:30:04 +0300

    refactor Entity

--- core/src/main/java/io/github/labyrinthgenerator/pages/game3d/entities/Entity.java
+++ core/src/main/java/io/github/labyrinthgenerator/pages/game3d/entities/Entity.java
@@ -13,33 +13,34 @@ import io.github.labyrinthgenerator.pages.game3d.utils.EntityManager;
 import java.util.Objects;
 
 public abstract class Entity {
-	public final GameScreen screen;
+    protected final int id;
+
+    public final GameScreen screen;
     public final EntityManager entMan;
     private final ChunkManager chunkMan;
 
     private final Vector3 position;
-    protected final int id;
-	protected boolean shouldTick = true;
-	protected boolean render2D = true;
-	protected boolean render3D = true;
-
     private volatile Chunk chunk;
+
+    protected boolean shouldTick = true;
+    protected boolean render2D = true;
+    protected boolean render3D = true;
 
     protected volatile boolean isTick = false;
     protected volatile long transactionId = -1;
     protected volatile boolean isDestroyed = false;
 
-	protected Entity collidedEntity = null;
+    protected Entity collidedEntity = null;
 
-	public Entity(Vector3 position, final GameScreen screen) {
+    public Entity(Vector3 position, final GameScreen screen) {
         this.position = position.cpy();
-		this.screen = screen;
+        this.screen = screen;
 
         entMan = screen.game.getEntMan();
-		id = entMan.assignId();
+        id = entMan.assignId();
         chunk = entMan.addEntityOnChunkTransactional(this.position.x, this.position.z, this);
         chunkMan = screen.game.getChunkMan();
-	}
+    }
 
     public void setPosition(Vector3 newPosition) {
         position.set(newPosition);
@@ -63,27 +64,26 @@ public abstract class Entity {
     }
 
     public synchronized void updateChunk() {
-        if (!isDestroyed) {
-            Vector2 worldSize = chunkMan.getWorldSize();
-            if (position.x < -0.5 || position.z < -0.5 ||
-                position.x > worldSize.x || position.z > worldSize.y) {
-                System.err.println("Entity id: " + id + " position " + position + " out of bounds.");
-                return;
-            }
-            if (chunk != chunkMan.get(chunk.x, chunk.z)) {
-                throw new RuntimeException("Entity id: " + id + ", method updateChunk(), " + chunk + " is invalid.");
-            }
-            Chunk newChunk = chunkMan.get(position.x, position.z);
-            if (newChunk == null) {
-                throw new NullPointerException("Chunk at position " + position + " is null.");
-            }
-            if (newChunk.equals(chunk)) {
-                throw new UnsupportedOperationException("Ent id " + id + " at position " + position + " " + chunk + " newChunk == chunk.");
-            }
-            entMan.updateEntityChunkTransactional(chunk, newChunk, this);
-            screen.game.getRectMan().updateEntityChunkIfExistsRectTransactional(chunk, newChunk, this);
-            chunk = newChunk;
+        if (isDestroyed) return;
+        Vector2 worldSize = chunkMan.getWorldSize();
+        if (position.x < -0.5 || position.z < -0.5 ||
+            position.x > worldSize.x || position.z > worldSize.y) {
+            System.err.println("Entity id: " + id + " position " + position + " out of bounds.");
+            return;
         }
+        if (chunk != chunkMan.get(chunk.x, chunk.z)) {
+            throw new RuntimeException("Entity id: " + id + ", method updateChunk(), " + chunk + " is invalid.");
+        }
+        Chunk newChunk = chunkMan.get(position.x, position.z);
+        if (newChunk == null) {
+            throw new NullPointerException("Chunk at position " + position + " is null.");
+        }
+        if (newChunk.equals(chunk)) {
+            throw new UnsupportedOperationException("Ent id " + id + " at position " + position + " " + chunk + " newChunk == chunk.");
+        }
+        entMan.updateEntityChunkTransactional(chunk, newChunk, this);
+        screen.game.getRectMan().updateEntityChunkIfExistsRectTransactional(chunk, newChunk, this);
+        chunk = newChunk;
     }
 
     public Vector3 getPositionImmutable() {
@@ -102,32 +102,33 @@ public abstract class Entity {
         return position.z;
     }
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void onCollision(final RectanglePlus otherRect) {
-		collidedEntity = entMan.getEntityFromId(otherRect.getConnectedEntityId());
-	}
+    public void onCollision(final RectanglePlus otherRect) {
+        collidedEntity = entMan.getEntityFromId(otherRect.getConnectedEntityId());
+    }
 
-	public void render3D(final ModelBatch mdlBatch, final Environment env, final float delta) {
-	}
+    public void render3D(final ModelBatch mdlBatch, final Environment env, final float delta) {
+    }
 
-	public boolean shouldRender3D() {
-		return render3D;
-	}
+    public boolean shouldRender3D() {
+        return render3D;
+    }
 
-	public boolean shouldTick() {
-		return shouldTick && !isDestroyed;
-	}
+    public boolean shouldTick() {
+        return shouldTick && !isDestroyed;
+    }
 
-	public void tick(final float delta) {
-	}
+    public void tick(final float delta) {
+    }
 
     public void beforeTick() {
         long transactionId = entMan.getTransactionId();
         if (!entMan.isTransaction() || this.transactionId != transactionId) this.transactionId = transactionId;
-        else throw new UnsupportedOperationException("Entity id: " + id + " has already been ticked in this transaction.");
+        else
+            throw new UnsupportedOperationException("Entity id: " + id + " has already been ticked in this transaction.");
         isTick = true;
     }
 
