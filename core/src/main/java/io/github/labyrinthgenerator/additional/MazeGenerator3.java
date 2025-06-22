aae2e32df72f570c9ec5f2569f0a86745f4377e9
myusername-dot <andrej.iljuhin@ddgcorp.ru>
2025-06-22 12:56:50 +0300

    final MazeGenerator3

--- core/src/main/java/io/github/labyrinthgenerator/additional/MazeGenerator3.java
+++ core/src/main/java/io/github/labyrinthgenerator/additional/MazeGenerator3.java
@@ -1,6 +1,6 @@
 package io.github.labyrinthgenerator.additional;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
@@ -37,11 +37,7 @@ public class MazeGenerator3 {
         int[] DY = {-1, 1, 0, 0};
         int[] OP = {Dirs.S.ordinal(), Dirs.N.ordinal(), Dirs.W.ordinal(), Dirs.E.ordinal()};
 
-        List<Integer> directions = new ArrayList<>();
-        directions.add(Dirs.N.ordinal());
-        directions.add(Dirs.S.ordinal());
-        directions.add(Dirs.E.ordinal());
-        directions.add(Dirs.W.ordinal());
+        List<Integer> directions = Arrays.asList(Dirs.N.ordinal(), Dirs.S.ordinal(), Dirs.E.ordinal(), Dirs.W.ordinal());
         Collections.shuffle(directions, random);
 
         for (int direction : directions) {
@@ -63,35 +59,44 @@ public class MazeGenerator3 {
             for (int x = 0; x < width; x++) {
                 System.out.print((grid[y][x] & Dirs.S.value) != 0 ? " " : "_");
                 if ((grid[y][x] & Dirs.E.value) != 0) {
-                    System.out.print(((grid[y][x] | grid[y][x + 1]) & Dirs.S.value) != 0 ? " " : "_");
+                    System.out.print(((grid[y][x] & grid[y][x + 1]) & Dirs.S.value) != 0 ? " " : "_");
                 } else {
                     System.out.print("|");
                 }
             }
             System.out.print("\n");
         }
-        System.out.println(new String(new char[width * 2 + 1]).replace("\0", "1"));
-        for (int y = 0; y < height; y++) {
-            for (int r = 0; r <= 1; r++) {
-                System.out.print(2);
-                for (int x = 0; x < width; x++) {
-                    System.out.print((grid[y][x] & Dirs.S.value) != 0 ? 0 : 1 - r);
-                    if ((grid[y][x] & Dirs.E.value) != 0) {
-                        System.out.print(((grid[y][x] | grid[y][x + 1]) & Dirs.S.value) != 0 ?
-                            (r == 1 && y < height - 1 && (grid[y + 1][x] & Dirs.E.value) == 0) ? 2 :
-                                0 : 1 - r);
-                    } else if (r == 0) {
-                        System.out.print(2);
-                    } else if (y > 0 && (grid[y - 1][x] & Dirs.E.value) == 0) {
-                        System.out.print(2);
-                    } else if (y < height - 1 && (grid[y + 1][x] & Dirs.E.value) == 0) { // check
-                        System.out.print(2);
-                    } else {
-                        System.out.print(0);
-                    }
+
+        int[][] gridPlus = new int[height + 1][width];
+        System.arraycopy(MazeGenerator3.grid, 0, gridPlus, 1, height);
+        Arrays.fill(gridPlus[0], Dirs.E.value);
+        gridPlus[0][width - 1] = Dirs.N.value;
+
+        int heightFin = height * 2 + 1, widthFin = width * 2 + 1;
+        int[][] gridFin = new int[heightFin][widthFin];
+        for (int yf = 0; yf < heightFin; yf++) {
+            int y = yf / 2;
+            gridFin[yf][0] = 2;
+            for (int x = 0; x < width; x++) {
+                gridFin[yf][x * 2 + 1] = (gridPlus[y][x] & Dirs.S.value) != 0 ? 0 : 1 - yf % 2;
+                if (yf % 2 == 0 && (gridPlus[y][x] & Dirs.E.value) != 0) {
+                    gridFin[yf][x * 2 + 2] = ((gridPlus[y][x] & gridPlus[y][x + 1]) & Dirs.S.value) != 0 ? 0 : 1;
+                } else if (yf % 2 == 0) {
+                    gridFin[yf][x * 2 + 2] = yf != height * 2 ? 2 : 1;
+                } else if ((gridPlus[y + 1][x] & (Dirs.E.value)) == 0) {
+                    gridFin[yf][x * 2 + 2] = 2;
+                } else {
+                    gridFin[yf][x * 2 + 2] = 0;
                 }
-                System.out.print("\n");
             }
+        }
+        gridFin[heightFin - 1][widthFin - 1] = 2;
+
+        for (int y = 0; y < heightFin; y++) {
+            for (int x = 0; x < widthFin; x++) {
+                System.out.print(gridFin[y][x]);
+            }
+            System.out.println();
         }
     }
 }
