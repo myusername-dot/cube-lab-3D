fd604ac1f54d505b06b0e2593c6af11379b7f297
myusername-dot <andrej.iljuhin@ddgcorp.ru>
2025-06-25 17:39:41 +0300

    update SpotLightFreeShader

--- core/src/main/java/io/github/labyrinthgenerator/pages/game3d/shaders/SpotLightFreeShader.java
+++ core/src/main/java/io/github/labyrinthgenerator/pages/game3d/shaders/SpotLightFreeShader.java
@@ -29,10 +29,13 @@ public class SpotLightFreeShader implements Shader {
             "uniform mat4 u_projTrans;\n" +
             "uniform float spotCutoff;\n" +
             "varying vec2 v_texCoords;\n" +
-            "varying vec4 v_color;\n" +
+            "varying vec4 spotColor;\n" +
+            "varying float v_distance;\n" + // Расстояние до камеры
+            "varying float v_height;\n" + // Передаем высоту
             "void main(void)\n" +
             "{\n" +
-            "    gl_Position = u_projTrans * u_worldTrans * a_position;\n" + // * gl_ModelViewMatrix
+            "    gl_Position = u_projTrans * u_worldTrans * a_position;\n" +
+            "    v_height = gl_Position.y; // Передаем высоту в фрагментный шейдер\n" +
             "\n" +
             "    vec3 lightPosition  = (gl_Position).xyz;\n" +
             "    vec3 spotDirection  = normalize(lightPosition.xyz + vec3(0,0,1)).xyz;\n" +
@@ -45,10 +48,13 @@ public class SpotLightFreeShader implements Shader {
             "\n" +
             "    v_texCoords = a_texCoord0;\n" +
             "    if (acos(angle) < radians(spotCutoff))\n" +
-            "       v_color = vec4(1,1,0,1); // lit (yellow)\n" +
+            "       spotColor = vec4(1,1,0.1,1); // lit (yellow)\n" +
             "    else\n" +
-            "       v_color = vec4(0,0,0,1); // unlit(black);\n" +
+            "       spotColor = vec4(0.1,0.1,0.1,1); // unlit(black);\n" +
+            "\n" +
+            "    v_distance = length(gl_Position.xyz);\n" + // Вычисляем расстояние
             "}";
+
     String fragmentShader =
         "#ifdef GL_ES \n" +
             "#define LOWP lowp\n" +
@@ -56,14 +62,24 @@ public class SpotLightFreeShader implements Shader {
             "#else\n" +
             "#define LOWP \n" +
             "#endif\n" +
+            "uniform mat4 u_worldTrans;\n" +
+            "uniform mat4 u_projTrans;\n" +
             "varying LOWP vec2 v_texCoords;\n" +
             "uniform sampler2D u_texture;\n" +
-            "varying vec4 v_color;\n" +
+            "varying vec4 spotColor;\n" +
+            "uniform vec4 fogColor;\n" + // Цвет тумана
+            "varying float v_distance;\n" +
+            "uniform float fogDensity;\n" + // Плотность тумана
+            "varying float v_height;\n" + // Передаем высоту
             "void main(void)\n" +
             "{\n" +
             "   vec4 c = texture2D(u_texture, v_texCoords);\n" +
-            "   gl_FragColor = c * 0.5 + v_color * 0.5;\n" +
+            "   float fogDistanceFactor = exp(-fogDensity * v_distance);\n" + // Вычисляем фактор тумана
+            "   float heightFactor = max(0.0, v_height * 2);\n" + // Чем ниже фрагмент, тем больше плотность, 0 y в центре камеры, положительные значения ниже
+            "   float fogFactor = clamp(fogDistanceFactor + heightFactor, 0.0, 1.0);\n" +
+            "   gl_FragColor = mix(mix(c, spotColor, 0.5), fogColor, clamp(fogFactor, 0.0, 0.8));\n" + // Интерполяция между цветом текстуры и цветом тумана
             "}";
+
 
     private RenderContext context;
     private int u_projTrans;
@@ -85,6 +101,11 @@ public class SpotLightFreeShader implements Shader {
         program.setUniformMatrix(u_projTrans, camera.combined);
         program.setUniformf("spotCutoff", cutoffAngle);
         program.setUniformi("u_texture", 0);
+
+        // Устанавливаем цвет тумана и его плотность
+        program.setUniformf("fogColor", new Color(0.1f, 0.1f, 0.1f, 1.0f)); // Цвет тумана (например, серый)
+        program.setUniformf("fogDensity", 1f); // Плотность тумана (можно настроить)
+
         context.begin();
         context.setDepthTest(GL20.GL_LEQUAL);
         context.setCullFace(GL20.GL_BACK);
