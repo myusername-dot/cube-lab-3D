381b80dfd1fa0a1fe246cc3de17e2df72138d52e
myusername-dot <andrej.iljuhin@ddgcorp.ru>
2025-06-29 12:09:26 +0300

    refactor game screens

--- core/src/main/java/io/github/labyrinthgenerator/pages/game3d/screens/PlayScreen.java
+++ core/src/main/java/io/github/labyrinthgenerator/pages/game3d/screens/PlayScreen.java
@@ -11,7 +11,6 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g3d.Environment;
-import com.badlogic.gdx.graphics.g3d.Shader;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -19,88 +18,81 @@ import io.github.labyrinthgenerator.pages.game2d.Labyrinth2D;
 import io.github.labyrinthgenerator.pages.game3d.CubeLab3D;
 import io.github.labyrinthgenerator.pages.game3d.constants.Constants;
 import io.github.labyrinthgenerator.pages.game3d.entities.player.Player;
-import io.github.labyrinthgenerator.pages.game3d.shaders.FogFreeShader;
-import io.github.labyrinthgenerator.pages.game3d.shaders.MyShaderProvider;
 import io.github.labyrinthgenerator.pages.game3d.shaders.SkyBoxShaderProgram;
 
 import static io.github.labyrinthgenerator.pages.game3d.constants.Constants.HALF_UNIT;
 
 public class PlayScreen extends GameScreen {
     private final TextureRegion skyBg;
-
     private final Environment env;
-
-    private final Color fogColor;
+    private Color fogColor;
 
     private boolean showGuiMenu = false;
     private boolean showExitDistance = false;
     private int guiMenuSelection = 0;
 
-    private final BitmapFont guiFont01_64;
-    private final BitmapFont guiFont01_32;
-    private final BitmapFont guiFont02_16;
+    private final BitmapFont guiFont64;
+    private final BitmapFont guiFont32;
     private final String headsupDead = "YOU DIED!";
     private final String optionContinue = "CONTINUE";
     private final String optionToMainMenu = "QUIT TO MAIN MENU";
     private final String selectedOptionMark = ">";
-    private String playerHp;
-    private final GlyphLayout glyphLayoutOptionContinue;
-    private final GlyphLayout glyphLayoutHeadsupDead;
-    private final GlyphLayout glyphLayoutOptionToMainMenu;
+    private GlyphLayout glyphLayoutOptionContinue;
+    private GlyphLayout glyphLayoutHeadsupDead;
+    private GlyphLayout glyphLayoutOptionToMainMenu;
 
     private final Sound sfxItem;
     private long sfxItemId;
-    //private final Sound musicBackground;
-    //private final long musicBackgroundId;
 
     private final SkyBoxShaderProgram envCubeMap;
 
     public PlayScreen(final CubeLab3D game) {
         super(game);
-
         viewport = new StretchViewport(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
+        env = createEnvironment();
+        skyBg = createSkyBackground(game);
+        guiFont64 = game.getAssMan().get(game.getAssMan().font03_64);
+        guiFont32 = game.getAssMan().get(game.getAssMan().font03_32);
 
-        env = new Environment();
-        env.set(new ColorAttribute(ColorAttribute.AmbientLight, 1f, 1f, 1f, 1f));
-        fogColor = new Color(66 / 256f, 33 / 256f, 54 / 256f, 1f);
-        //env.set(new ColorAttribute(ColorAttribute.Fog, fogColor));
-
-        skyBg = new TextureRegion((Texture) game.getAssMan().get(game.getAssMan().bgSky01));
-        skyBg.flip(false, true);
-
-        guiFont01_64 = game.getAssMan().get(game.getAssMan().font03_64);
-        guiFont01_32 = game.getAssMan().get(game.getAssMan().font03_32);
-        guiFont02_16 = game.getAssMan().get(game.getAssMan().font02_16);
-        glyphLayoutOptionContinue = new GlyphLayout(guiFont01_64, optionContinue);
-        glyphLayoutOptionToMainMenu = new GlyphLayout(guiFont01_64, optionToMainMenu);
-        glyphLayoutHeadsupDead = new GlyphLayout(guiFont01_64, headsupDead);
+        createGlyphLayouts(); // Инициализация GlyphLayout
 
         sfxItem = game.getAssMan().get(game.getAssMan().sfxItem);
-        //musicBackground = game.getAssMan().get(game.getAssMan().musicBackground01);
-
         game.getMapBuilder().buildMap(Labyrinth2D.txtFilename);
 
-        float playerRectWidth = (HALF_UNIT / 2f);
-        float playerRectDepth = (HALF_UNIT / 2f);
-        Vector3 playerSpawnPosition = new Vector3(
-            game.getMapBuilder().mapLoadSpawnPosition.x + HALF_UNIT - playerRectWidth / 2f,
-            0,
-            game.getMapBuilder().mapLoadSpawnPosition.y + HALF_UNIT - playerRectDepth / 2f
-        );
-        player = new Player(playerSpawnPosition, playerRectWidth, playerRectDepth, this);
+        Vector3 playerSpawnPosition = getPlayerSpawnPosition(game);
+        player = new Player(playerSpawnPosition, HALF_UNIT / 2f, HALF_UNIT / 2f, this);
         setCurrentCam(player.playerCam);
         viewport.setCamera(currentCam);
 
         Gdx.input.setCursorCatched(true);
-
-        //musicBackgroundId = musicBackground.loop(game.getMusicVolume());
-
         envCubeMap = new SkyBoxShaderProgram(new Pixmap(Gdx.files.internal(game.getAssMan().bgSky01)));
     }
 
-    @Override
-    public void dispose() {
-        super.dispose();
+    private Environment createEnvironment() {
+        Environment environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 1f, 1f, 1f, 1f));
+        fogColor = new Color(66 / 256f, 33 / 256f, 54 / 256f, 1f);
+        return environment;
+    }
+
+    private TextureRegion createSkyBackground(final CubeLab3D game) {
+        TextureRegion textureRegion = new TextureRegion((Texture) game.getAssMan().get(game.getAssMan().bgSky01));
+        textureRegion.flip(false, true);
+        return textureRegion;
+    }
+
+    private Vector3 getPlayerSpawnPosition(final CubeLab3D game) {
+        return new Vector3(
+            game.getMapBuilder().mapLoadSpawnPosition.x + HALF_UNIT - (HALF_UNIT / 2f) / 2f,
+            0,
+            game.getMapBuilder().mapLoadSpawnPosition.y + HALF_UNIT - (HALF_UNIT / 2f) / 2f
+        );
+    }
+
+    private void createGlyphLayouts() {
+        glyphLayoutOptionContinue = new GlyphLayout(guiFont64, optionContinue);
+        glyphLayoutOptionToMainMenu = new GlyphLayout(guiFont64, optionToMainMenu);
+        glyphLayoutHeadsupDead = new GlyphLayout(guiFont64, headsupDead);
     }
 
     @Override
@@ -111,58 +103,63 @@ public class PlayScreen extends GameScreen {
             showGuiMenu = true;
         }
 
-        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) { // For easy quit while debugging.
-            showGuiMenu = !showGuiMenu;
-            Gdx.input.setCursorCatched(!showGuiMenu);
-            guiMenuSelection = 0;
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+            toggleGuiMenu();
         }
 
-        if (Gdx.input.isKeyJustPressed(Input.Keys.M)) { // For easy quit while debugging.
+        if (Gdx.input.isKeyJustPressed(Input.Keys.M)) {
             showExitDistance = !showExitDistance;
         }
 
         if (showGuiMenu) {
-            game.gameIsPaused = true;
-
-            if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
-                guiMenuSelection--;
-                limitGuiSelection();
-                playItemSound();
-            }
-
-            if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) {
-                guiMenuSelection++;
-                limitGuiSelection();
-                playItemSound();
-            }
-
-            if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER) || Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
-                playItemSound();
-                switch (guiMenuSelection) {
-                    case 0:
-//					continue
-                        Gdx.input.setCursorCatched(true);
-                        showGuiMenu = false;
-                        break;
-                    case 1:
-//					to menu screen
-                        //musicBackground.stop(musicBackgroundId);
-
-                        removeAllEntities();
-                        game.setScreen(new MainMenuScreen(game));
-                        break;
-                    default:
-                        break;
-                }
-            }
+            handleGuiMenuInput();
         } else {
             game.gameIsPaused = false;
-        }
-
-        if (!game.gameIsPaused) {
             if (!player.isDead) {
                 player.handleInput(delta);
             }
+        }
+    }
+
+    private void toggleGuiMenu() {
+        showGuiMenu = !showGuiMenu;
+        Gdx.input.setCursorCatched(!showGuiMenu);
+        guiMenuSelection = 0;
+    }
+
+    private void handleGuiMenuInput() {
+        game.gameIsPaused = true;
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
+            guiMenuSelection--;
+            limitGuiSelection();
+            playItemSound();
+        }
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) {
+            guiMenuSelection++;
+            limitGuiSelection();
+            playItemSound();
+        }
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER) || Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            playItemSound();
+            handleMenuSelection();
+        }
+    }
+
+    private void handleMenuSelection() {
+        switch (guiMenuSelection) {
+            case 0: // Continue
+                Gdx.input.setCursorCatched(true);
+                showGuiMenu = false;
+                break;
+            case 1: // Quit to Main Menu
+                removeAllEntities();
+                game.setScreen(new MainMenuScreen(game));
+                break;
+            default:
+                break;
         }
     }
 
@@ -181,69 +178,85 @@ public class PlayScreen extends GameScreen {
     @Override
     public void render(final float delta) {
         super.render(delta);
-
         currentCam.update();
 
         game.getFbo().begin();
+        clearScreen();
+        envCubeMap.render(currentCam);
+        renderEntities(delta);
+        game.getFbo().end();
+
+        renderFinalFbo();
+    }
+
+    private void clearScreen() {
         Gdx.gl.glClearColor(fogColor.r, fogColor.g, fogColor.b, fogColor.a);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+    }
 
-        envCubeMap.render(currentCam);
-
+    private void renderEntities(float delta) {
         game.getMdlBatch().begin(currentCam);
         game.getEntMan().render3DAllEntities(game.getMdlBatch(), env, delta, currentCam.position.x, currentCam.position.z);
         game.getMdlBatch().end();
-        game.getFbo().end();
+    }
 
-//		render final fbo
+    private void renderFinalFbo() {
         game.getBatch().begin();
+        game.getBatch().draw(game.getFbo().getColorBufferTexture(), 0, 0, viewport.getWorldWidth(), viewport.getWorldHeight());
+        guiFont32.draw(game.getBatch(), "fps: " + Gdx.graphics.getFramesPerSecond(), viewport.getWorldWidth() / 8f, viewport.getWorldHeight() - 16);
+        renderDebugInfo();
+        renderGuiMenu();
+        game.getBatch().end();
+    }
 
-        game.getBatch().draw(game.getFbo().getColorBufferTexture(), 0, 0, viewport.getWorldWidth(),
-            viewport.getWorldHeight());
-
-        guiFont01_32.draw(game.getBatch(), "fps: " + Gdx.graphics.getFramesPerSecond(), viewport.getWorldWidth() / 8f, viewport.getWorldHeight() - 16);
-
+    private void renderDebugInfo() {
         if (showExitDistance && !showGuiMenu) {
-            guiFont01_32.draw(game.getBatch(), "velocity          : " + player.getVelocity(), viewport.getWorldWidth() / 8f, viewport.getWorldHeight() - 32);
-            guiFont01_32.draw(game.getBatch(), "velocity forward: " + player.getVelocity(), viewport.getWorldWidth() / 8f, viewport.getWorldHeight() - 48);
-
-
-            guiFont01_32.draw(game.getBatch(), "Exit distance: " + player.getExitDistance(), viewport.getWorldWidth() / 8f, viewport.getWorldHeight() - 64);
+            guiFont32.draw(game.getBatch(), "velocity          : " + player.getVelocity(), viewport.getWorldWidth() / 8f, viewport.getWorldHeight() - 32);
+            guiFont32.draw(game.getBatch(), "velocity forward: " + player.getVelocity(), viewport.getWorldWidth() / 8f, viewport.getWorldHeight() - 48);
+            guiFont32.draw(game.getBatch(), "Exit distance: " + player.getExitDistance(), viewport.getWorldWidth() / 8f, viewport.getWorldHeight() - 64);
         }
+    }
 
-//		gui menu
+    private void renderGuiMenu() {
         if (showGuiMenu) {
             if (player.isDead) {
-                final float headsupDeadX = viewport.getWorldWidth() / 2f - glyphLayoutHeadsupDead.width / 2f;
-                guiFont01_64.draw(game.getBatch(), headsupDead, headsupDeadX, 188 + 64);
+                float headsupDeadX = viewport.getWorldWidth() / 2f - glyphLayoutHeadsupDead.width / 2f;
+                guiFont64.draw(game.getBatch(), headsupDead, headsupDeadX, 188 + 64);
             }
 
-            final float optionContinueX = viewport.getWorldWidth() / 2f - glyphLayoutOptionContinue.width / 2f;
-            if (!player.isDead) {
-                guiFont01_64.draw(game.getBatch(), optionContinue, optionContinueX, 188);
-            }
-
-            final float optionMainMenuX = viewport.getWorldWidth() / 2f - glyphLayoutOptionToMainMenu.width / 2f;
-            guiFont01_64.draw(game.getBatch(), optionToMainMenu, optionMainMenuX, 188 - 32);
+            drawMenuOption(optionContinue, glyphLayoutOptionContinue, 188, !player.isDead);
+            drawMenuOption(optionToMainMenu, glyphLayoutOptionToMainMenu, 188 - 32, true);
 
             if (player.isDead) {
                 guiMenuSelection = 1;
             }
 
-            switch (guiMenuSelection) {
-                case 0:
-                    guiFont01_64.draw(game.getBatch(), selectedOptionMark, optionContinueX - 32, 188);
-                    break;
-                case 1:
-                    guiFont01_64.draw(game.getBatch(), selectedOptionMark, optionMainMenuX - 32, 188 - 32);
-                    break;
-                default:
-                    guiFont01_64.draw(game.getBatch(), selectedOptionMark, optionContinueX - 32, 188);
-                    break;
-            }
+            drawSelectedOptionMarker();
         }
+    }
 
-        game.getBatch().end();
+    private void drawMenuOption(String optionText, GlyphLayout layout, float y, boolean visible) {
+        if (visible) {
+            float optionX = viewport.getWorldWidth() / 2f - layout.width / 2f;
+            guiFont64.draw(game.getBatch(), optionText, optionX, y);
+        }
+    }
+
+    private void drawSelectedOptionMarker() {
+        float optionContinueX = viewport.getWorldWidth() / 2f - glyphLayoutOptionContinue.width / 2f;
+        float optionMainMenuX = viewport.getWorldWidth() / 2f - glyphLayoutOptionToMainMenu.width / 2f;
+
+        switch (guiMenuSelection) {
+            case 0:
+                guiFont64.draw(game.getBatch(), selectedOptionMark, optionContinueX - 32, 188);
+                break;
+            case 1:
+                guiFont64.draw(game.getBatch(), selectedOptionMark, optionMainMenuX - 32, 188 - 32);
+                break;
+            default:
+                guiFont64.draw(game.getBatch(), selectedOptionMark, optionContinueX - 32, 188);
+                break;
+        }
     }
 
     @Override
