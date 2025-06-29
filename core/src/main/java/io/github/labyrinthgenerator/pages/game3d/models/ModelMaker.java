0bc17024aa42f97ba879b1fa168dc25b89f191ba
myusername-dot <andrej.iljuhin@ddgcorp.ru>
2025-06-29 13:08:26 +0300

    refactor model maker

--- core/src/main/java/io/github/labyrinthgenerator/pages/game3d/models/ModelMaker.java
+++ core/src/main/java/io/github/labyrinthgenerator/pages/game3d/models/ModelMaker.java
@@ -18,177 +18,105 @@ import static io.github.labyrinthgenerator.pages.game3d.constants.Constants.TEXT
 public class ModelMaker {
 
     final CubeLab3D game;
-
     private final ModelBuilder mdlBuilder;
 
-    public Model mdlGrid = new Model();
-
-    public Model mdlWallNorth = new Model();
-    public Model mdlWallSouth = new Model();
-    public Model mdlWallWest = new Model();
-    public Model mdlWallEast = new Model();
-    public Model mdlFloor = new Model();
-    public Model mdlCeiling = new Model();
-
-    public Model mdlEnemy = new Model();
-    public Model mdlPoint = new Model();
+    public Model mdlGrid;
+    public Model mdlWallNorth;
+    public Model mdlWallSouth;
+    public Model mdlWallWest;
+    public Model mdlWallEast;
+    public Model mdlFloor;
+    public Model mdlCeiling;
+    public Model mdlEnemy;
+    public Model mdlPoint;
 
     public ModelMaker(final CubeLab3D game) {
         this.game = game;
-
-        mdlBuilder = new ModelBuilder();
-
+        this.mdlBuilder = new ModelBuilder();
         buildModels();
     }
 
     private void buildModels() {
         mdlGrid = mdlBuilder.createLineGrid(10, 10, 1, 1, new Material(), Usage.Position | Usage.Normal);
 
-//		ENEMY
-//			final TextureRegion texRegNorth = new TextureRegion((Texture) game.getAssMan().get(game.getAssMan().atlas01),
-//					TEXTURE_SIZE * 2, TEXTURE_SIZE, -TEXTURE_SIZE, TEXTURE_SIZE); // flip x
+        mdlEnemy = createModelWithTexture(HALF_UNIT, HALF_UNIT, 0, -HALF_UNIT, HALF_UNIT, 0, -HALF_UNIT,
+            -HALF_UNIT, 0, HALF_UNIT, -HALF_UNIT, 0, 0, 0, -1, getTextureMaterial(TextureAttribute.Diffuse));
 
-        final TextureAttribute taEnemy = new TextureAttribute(TextureAttribute.Diffuse);
-        taEnemy.textureDescription.minFilter = TextureFilter.Nearest;
-        taEnemy.textureDescription.magFilter = TextureFilter.Nearest;
+        mdlPoint = createModelWithTexture(HALF_UNIT / 6f, HALF_UNIT / 6f, 0, -HALF_UNIT / 6f,
+            HALF_UNIT / 6f, 0, -HALF_UNIT / 6f, -HALF_UNIT / 6f, 0, HALF_UNIT / 6f, -HALF_UNIT / 6f, 0,
+            0, 0, -1, getTextureMaterial(TextureAttribute.Diffuse));
 
-        final Material matEnemy = new Material();
-        matEnemy.set(taEnemy);
+        mdlWallNorth = createWallModel(0, TEXTURE_SIZE, "NORTH");
+        mdlWallSouth = createWallModel(TEXTURE_SIZE * 3, TEXTURE_SIZE, "SOUTH");
+        mdlWallWest = createWallModel(0, TEXTURE_SIZE, "WEST");
+        mdlWallEast = createWallModel(TEXTURE_SIZE * 2, TEXTURE_SIZE, "EAST");
 
-        mdlEnemy = mdlBuilder.createRect(HALF_UNIT, HALF_UNIT, 0, -HALF_UNIT,
+        mdlFloor = createFloorOrCeilingModel(TEXTURE_SIZE, TEXTURE_SIZE * 2, 1);
+        mdlCeiling = createFloorOrCeilingModel(TEXTURE_SIZE, 0, -1);
+    }
+
+    private Model createWallModel(int textureX, int textureY, String direction) {
+        Texture texture = textureRegionToTexture((Texture) game.getAssMan().get(game.getAssMan().atlas01),
+            textureX, textureY, TEXTURE_SIZE, TEXTURE_SIZE);
+        Material material = getTextureMaterial(TextureAttribute.Diffuse, texture);
+        Model wallModel = mdlBuilder.createRect(HALF_UNIT, HALF_UNIT, 0, -HALF_UNIT,
             HALF_UNIT, 0, -HALF_UNIT, -HALF_UNIT, 0, HALF_UNIT,
-            -HALF_UNIT, 0, 0, 0, -1, matEnemy, Usage.Position | Usage.Normal | Usage.TextureCoordinates);
+            -HALF_UNIT, 0, 0, 0, -1, material, Usage.Position | Usage.Normal | Usage.TextureCoordinates);
 
-// POINT
-        final TextureAttribute taPoint = new TextureAttribute(TextureAttribute.Diffuse);
-        taPoint.textureDescription.minFilter = TextureFilter.Nearest;
-        taPoint.textureDescription.magFilter = TextureFilter.Nearest;
+        switch (direction) {
+            case "SOUTH":
+                wallModel.nodes.get(0).rotation.set(Vector3.Y, 180f);
+                break;
+            case "WEST":
+                wallModel.nodes.get(0).rotation.set(Vector3.Y, -90f);
+                break;
+            case "EAST":
+                wallModel.nodes.get(0).rotation.set(Vector3.Y, 90f);
+                break;
+        }
+        return wallModel;
+    }
 
-        final Material matPoint = new Material();
-        matPoint.set(taPoint);
-
-        mdlPoint = mdlBuilder.createRect(HALF_UNIT / 6f, HALF_UNIT / 6f, 0, -HALF_UNIT / 6f,
-            HALF_UNIT / 6f, 0, -HALF_UNIT / 6f, -HALF_UNIT / 6f, 0, HALF_UNIT / 6f,
-            -HALF_UNIT / 6f, 0, 0, 0, -1, matEnemy, Usage.Position | Usage.Normal | Usage.TextureCoordinates);
-
-//	NORTH WALL
-        final Texture texRegNorth = textureRegionToTexture((Texture) game.getAssMan().get(game.getAssMan().atlas01),
-            TEXTURE_SIZE, TEXTURE_SIZE, TEXTURE_SIZE, TEXTURE_SIZE);
-
-        final TextureAttribute taNorth = new TextureAttribute(TextureAttribute.Diffuse, texRegNorth);
-        taNorth.textureDescription.minFilter = TextureFilter.Nearest;
-        taNorth.textureDescription.magFilter = TextureFilter.Nearest;
-
-        final Material matNorth = new Material();
-        matNorth.set(taNorth);
-
-        mdlWallNorth = mdlBuilder.createRect(HALF_UNIT, HALF_UNIT, 0, -HALF_UNIT,
+    private Model createFloorOrCeilingModel(int textureX, int textureY, int direction) {
+        Texture texture = textureRegionToTexture((Texture) game.getAssMan().get(game.getAssMan().atlas01),
+            textureX, textureY, TEXTURE_SIZE, TEXTURE_SIZE);
+        Material material = getTextureMaterial(TextureAttribute.Diffuse, texture);
+        Model model = mdlBuilder.createRect(HALF_UNIT, HALF_UNIT, 0, -HALF_UNIT,
             HALF_UNIT, 0, -HALF_UNIT, -HALF_UNIT, 0, HALF_UNIT,
-            -HALF_UNIT, 0, 0, 0, -1, matNorth, Usage.Position | Usage.Normal | Usage.TextureCoordinates);
+            -HALF_UNIT, 0, 0, direction, 0, material, Usage.Position | Usage.Normal | Usage.TextureCoordinates);
 
-//	mdlInstWallNorth = new ModelInstance(mdlWallNorth);
+        model.nodes.get(0).rotation.set(Vector3.X, direction == 1 ? 90f : -90f);
+        return model;
+    }
 
-//	SOUTH WALL
-        final Texture texRegSouth = textureRegionToTexture((Texture) game.getAssMan().get(game.getAssMan().atlas01),
-            TEXTURE_SIZE * 3, TEXTURE_SIZE, TEXTURE_SIZE, TEXTURE_SIZE); // flip x
-
-        final TextureAttribute taSouth = new TextureAttribute(TextureAttribute.Diffuse, texRegSouth);
-        taSouth.textureDescription.minFilter = TextureFilter.Nearest;
-        taSouth.textureDescription.magFilter = TextureFilter.Nearest;
-
-        final Material matSouth = new Material();
-        matSouth.set(taSouth);
-
-        mdlWallSouth = mdlBuilder.createRect(HALF_UNIT, HALF_UNIT, 0, -HALF_UNIT,
-            HALF_UNIT, 0, -HALF_UNIT, -HALF_UNIT, 0, HALF_UNIT,
-            -HALF_UNIT, 0, 0, 0, -1, matSouth, Usage.Position | Usage.Normal | Usage.TextureCoordinates);
-        mdlWallSouth.nodes.get(0).rotation.set(Vector3.Y, 180f);
-
-//	mdlInstWallSouth = new ModelInstance(mdlWallSouth);
-
-//	WEST WALL
-        final Texture texRegWest = textureRegionToTexture((Texture) game.getAssMan().get(game.getAssMan().atlas01),
-            0, TEXTURE_SIZE, TEXTURE_SIZE, TEXTURE_SIZE);
-
-        final TextureAttribute taWest = new TextureAttribute(TextureAttribute.Diffuse, texRegWest);
-        taWest.textureDescription.minFilter = TextureFilter.Nearest;
-        taWest.textureDescription.magFilter = TextureFilter.Nearest;
-
-        final Material matWest = new Material();
-        matWest.set(taWest);
-
-        mdlWallWest = mdlBuilder.createRect(HALF_UNIT, HALF_UNIT, 0, -HALF_UNIT,
-            HALF_UNIT, 0, -HALF_UNIT, -HALF_UNIT, 0, HALF_UNIT,
-            -HALF_UNIT, 0, 0, 0, -1, matWest, Usage.Position | Usage.Normal | Usage.TextureCoordinates);
-        mdlWallWest.nodes.get(0).rotation.set(Vector3.Y, -90f);
-
-//	mdlInstWallWest = new ModelInstance(mdlWallWest);
-
-//	EAST WALL
-        final Texture texRegEast = textureRegionToTexture((Texture) game.getAssMan().get(game.getAssMan().atlas01),
-            TEXTURE_SIZE * 2, TEXTURE_SIZE, TEXTURE_SIZE, TEXTURE_SIZE); // flip y
-
-        final TextureAttribute taEast = new TextureAttribute(TextureAttribute.Diffuse, texRegEast);
-        taEast.textureDescription.minFilter = TextureFilter.Nearest;
-        taEast.textureDescription.magFilter = TextureFilter.Nearest;
-
-        final Material matEast = new Material();
-        matEast.set(taEast);
-
-        mdlWallEast = mdlBuilder.createRect(HALF_UNIT, HALF_UNIT, 0, -HALF_UNIT,
-            HALF_UNIT, 0, -HALF_UNIT, -HALF_UNIT, 0, HALF_UNIT,
-            -HALF_UNIT, 0, 0, 0, -1, matEast, Usage.Position | Usage.Normal | Usage.TextureCoordinates);
-        mdlWallEast.nodes.get(0).rotation.set(Vector3.Y, 90f);
-
-//	mdlInstWallEast = new ModelInstance(mdlWallEast);
-
-//	FLOOR
-        final Texture texRegFloor = textureRegionToTexture((Texture) game.getAssMan().get(game.getAssMan().atlas01),
-            TEXTURE_SIZE, TEXTURE_SIZE * 2, TEXTURE_SIZE, TEXTURE_SIZE); // flip x
-
-        final TextureAttribute taFloor = new TextureAttribute(TextureAttribute.Diffuse, texRegFloor);
-        taFloor.textureDescription.minFilter = TextureFilter.Nearest;
-        taFloor.textureDescription.magFilter = TextureFilter.Nearest;
-
-        final Material matFloor = new Material();
-        matFloor.set(taFloor);
-
-        mdlFloor = mdlBuilder.createRect(HALF_UNIT, HALF_UNIT, 0, -HALF_UNIT,
-            HALF_UNIT, 0, -HALF_UNIT, -HALF_UNIT, 0, HALF_UNIT,
-            -HALF_UNIT, 0, 0, 1, 0, matFloor, Usage.Position | Usage.Normal | Usage.TextureCoordinates);
-        mdlFloor.nodes.get(0).rotation.set(Vector3.X, 90f);
-
-//	mdlInstFloor = new ModelInstance(mdlFloor);
-
-//		CEILING
-        final Texture texRegCeiling = textureRegionToTexture((Texture) game.getAssMan().get(game.getAssMan().atlas01),
-            TEXTURE_SIZE, 0, TEXTURE_SIZE, TEXTURE_SIZE); // flip x
-
-        final TextureAttribute taCeiling = new TextureAttribute(TextureAttribute.Diffuse, texRegCeiling);
-        taCeiling.textureDescription.minFilter = TextureFilter.Nearest;
-        taCeiling.textureDescription.magFilter = TextureFilter.Nearest;
-
-        final Material matCeiling = new Material();
-        matCeiling.set(taCeiling);
-
-        mdlCeiling = mdlBuilder.createRect(HALF_UNIT, HALF_UNIT, 0, -HALF_UNIT,
-            HALF_UNIT, 0, -HALF_UNIT, -HALF_UNIT, 0, HALF_UNIT,
-            -HALF_UNIT, 0, 0, -1, 0, matCeiling,
+    private Model createModelWithTexture(float x1, float y1, float z1, float x2, float y2, float z2, float x3,
+                                         float y3, float z3, float x4, float y4, float z4, float nx, float ny,
+                                         float nz, Material material) {
+        return mdlBuilder.createRect(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4, nx, ny, nz, material,
             Usage.Position | Usage.Normal | Usage.TextureCoordinates);
-        mdlCeiling.nodes.get(0).rotation.set(Vector3.X, -90f); // not totally correct. Should flip Y. Maybe not needed.
+    }
 
-//		mdlInstCeiling = new ModelInstance(mdlCeiling);
+    private Material getTextureMaterial(long attributeType) {
+        return getTextureMaterial(attributeType, null);
+    }
+
+    private Material getTextureMaterial(long attributeType, Texture texture) {
+        TextureAttribute textureAttribute = new TextureAttribute(attributeType, texture);
+        textureAttribute.textureDescription.minFilter = TextureFilter.Nearest;
+        textureAttribute.textureDescription.magFilter = TextureFilter.Nearest;
+
+        Material material = new Material();
+        material.set(textureAttribute);
+        return material;
     }
 
     public static Texture textureRegionToTexture(Texture textureRegion, int x, int y, int width, int height) {
         Pixmap pixmap = new Pixmap(width, height, Pixmap.Format.RGBA8888);
-
         TextureData textureData = textureRegion.getTextureData();
         if (!textureData.isPrepared()) textureData.prepare();
         Pixmap texturePixmap = textureData.consumePixmap();
 
         pixmap.drawPixmap(texturePixmap, 0, 0, x, y, width, height);
-
         Texture newTexture = new Texture(pixmap);
 
         texturePixmap.dispose();
