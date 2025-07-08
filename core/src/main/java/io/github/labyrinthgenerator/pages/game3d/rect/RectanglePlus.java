6fbb22ac868f2d1814ae9483d7ee770a2fcf5d81
myusername-dot <andrej.iljuhin@ddgcorp.ru>
2025-07-08 15:15:20 +0300

    add rectangle to string

--- core/src/main/java/io/github/labyrinthgenerator/pages/game3d/rect/RectanglePlus.java
+++ core/src/main/java/io/github/labyrinthgenerator/pages/game3d/rect/RectanglePlus.java
@@ -115,4 +115,9 @@ public class RectanglePlus {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+    @Override
+    public String toString() {
+        return "[" + x + "," + y + "," + z + ";" + width + "," + height + "," + depth + ']';
+    }
 }
