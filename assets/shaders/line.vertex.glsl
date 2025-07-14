082f3d82445049e9965d8a3df911ea9256f0e738
myusername-dot <andrej.iljuhin@ddgcorp.ru>
2025-07-14 21:20:23 +0300

    add mesh to RectanglePlus

--- assets/shaders/line.vertex.glsl
+++ assets/shaders/line.vertex.glsl
@@ -4,28 +4,13 @@ layout(location = 0) in vec3 a_Position;
 layout(location = 1) in vec4 a_Color;
 
 uniform mat4 u_projectionView;
+uniform mat4 u_worldTrans;
+uniform vec4 u_Color;
 
 out vec4 v_Color;
 
 void main() {
     // Инвертируем Y-координату
-    gl_Position = u_projectionView * vec4(a_Position.x, -a_Position.y, a_Position.z, 1.0);
-    v_Color = a_Color;
+    gl_Position = u_projectionView * u_worldTrans * vec4(a_Position.x, a_Position.y, a_Position.z, 1.0);
+    v_Color = u_Color;
 }
-
-//#version 330 core
-
-//layout(location = 0) in vec3 a_Position;
-//layout(location = 1) in vec4 a_Color;
-
-//uniform mat4 u_projectionView;
-//uniform mat4 u_worldTrans;
-
-//out vec4 v_Color;
-
-//void main() {
-//    // Инвертируем Y-координату
-//    vec4 worldPosition = u_worldTrans * vec4(a_Position.x, -a_Position.y, a_Position.z, 1.0);
-//    gl_Position = u_projectionView * worldPosition;
-//    v_Color = a_Color;
-//}
--- core/src/main/java/io/github/labyrinthgenerator/pages/game3d/debug/MyDebugRenderer.java
+++ core/src/main/java/io/github/labyrinthgenerator/pages/game3d/debug/MyDebugRenderer.java
@@ -1,7 +1,10 @@
 package io.github.labyrinthgenerator.pages.game3d.debug;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Mesh;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.utils.GdxRuntimeException;
@@ -24,13 +27,11 @@ public class MyDebugRenderer {
     public DebugMode debugMode = DebugMode.DISABLE;
 
     private ShaderProgram shader;
-    private Mesh lineMesh;
     private final CubeLab3D game;
 
     public MyDebugRenderer(CubeLab3D game) {
         this.game = game;
         createShader();
-        createMesh();
     }
 
     private void createShader() {
@@ -42,20 +43,12 @@ public class MyDebugRenderer {
         }
     }
 
-    private void createMesh() {
-        lineMesh = new Mesh(true, 1_000_000, 0,
-            new VertexAttribute(VertexAttributes.Usage.Position, 3, "a_Position"),
-            new VertexAttribute(VertexAttributes.Usage.ColorPacked, 4, "a_Color"));
-    }
-
     public void render(Camera camera) {
         Matrix4 combined = camera.combined.cpy();
 
-        // Устанавливаем матрицу проекции
         shader.bind();
         shader.setUniformMatrix("u_projectionView", combined);
 
-        // Рендерим линии
         renderLines();
 
         shader.end();
@@ -67,160 +60,16 @@ public class MyDebugRenderer {
 
         for (RectanglePlus rect : shapes) {
             // Получаем матрицу трансформации для текущего прямоугольника
-            //Matrix4 worldTrans = rect.getTransformMatrix();
+            Matrix4 worldTrans = rect.getTransformMatrix();
+            shader.setUniformMatrix("u_worldTrans", worldTrans);
+            Color color = rect.overlaps ? JOINT_COLOR : SHAPE_COLOR;
+            shader.setUniformf("u_Color", color);
+            Mesh rectMesh = rect.getMesh();
 
-            //shader.setUniformMatrix("u_worldTrans", worldTrans);
-
-            lineMesh.setVertices(getLineVertices(rect));
-
-            lineMesh.render(shader, GL20.GL_LINES);
+            rectMesh.render(shader, GL20.GL_LINES);
         }
 
         Gdx.gl.glDisable(GL20.GL_BLEND);
-    }
-
-    private float[] getLineVertices(RectanglePlus rectanglePlus) {
-        // Каждая прямоугольная форма добавляет 12 линий (по 2 вершины на линию)
-        // 12 линий * 2 вершины * 7 значений (3 позиции и 4 цвета)
-        float[] vertices = new float[12 * 2 * 7]; // 12 линий * 2 вершины * 7 значений
-        int index = 0;
-
-        Color color = rectanglePlus.overlaps ? JOINT_COLOR : SHAPE_COLOR;
-
-        // Добавляем вершины для рендеринга 3D прямоугольника
-        addRectangleVertices(rectanglePlus, color, vertices, index);
-
-        return vertices;
-    }
-
-    private void addRectangleVertices(RectanglePlus rectanglePlus, Color color, float[] vertices, int index) {
-        float x = rectanglePlus.getX();
-        float y = rectanglePlus.getY();
-        float z = rectanglePlus.getZ();
-        float width = rectanglePlus.getWidth();
-        float height = rectanglePlus.getHeight();
-        float depth = rectanglePlus.getDepth();
-
-        // Добавляем вершины для линий (по периметру)
-        // Front bottom
-        vertices[index++] = x;
-        vertices[index++] = y;
-        vertices[index++] = z;
-        vertices[index++] = color.toFloatBits();
-        vertices[index++] = x + width;
-        vertices[index++] = y;
-        vertices[index++] = z;
-        vertices[index++] = color.toFloatBits();
-
-        // Right bottom
-        vertices[index++] = x + width;
-        vertices[index++] = y;
-        vertices[index++] = z;
-        vertices[index++] = color.toFloatBits();
-        vertices[index++] = x + width;
-        vertices[index++] = y;
-        vertices[index++] = z + depth;
-        vertices[index++] = color.toFloatBits();
-
-        // Back bottom
-        vertices[index++] = x + width;
-        vertices[index++] = y;
-        vertices[index++] = z + depth;
-        vertices[index++] = color.toFloatBits();
-        vertices[index++] = x;
-        vertices[index++] = y;
-        vertices[index++] = z + depth;
-        vertices[index++] = color.toFloatBits();
-
-        // Left bottom
-        vertices[index++] = x;
-        vertices[index++] = y;
-        vertices[index++] = z + depth;
-        vertices[index++] = color.toFloatBits();
-        vertices[index++] = x;
-        vertices[index++] = y;
-        vertices[index++] = z;
-        vertices[index++] = color.toFloatBits();
-
-        // Front top
-        vertices[index++] = x;
-        vertices[index++] = y + height;
-        vertices[index++] = z;
-        vertices[index++] = color.toFloatBits();
-        vertices[index++] = x + width;
-        vertices[index++] = y + height;
-        vertices[index++] = z;
-        vertices[index++] = color.toFloatBits();
-
-        // Right top
-        vertices[index++] = x + width;
-        vertices[index++] = y + height;
-        vertices[index++] = z;
-        vertices[index++] = color.toFloatBits();
-        vertices[index++] = x + width;
-        vertices[index++] = y + height;
-        vertices[index++] = z + depth;
-        vertices[index++] = color.toFloatBits();
-
-        // Back top
-        vertices[index++] = x + width;
-        vertices[index++] = y + height;
-        vertices[index++] = z + depth;
-        vertices[index++] = color.toFloatBits();
-        vertices[index++] = x;
-        vertices[index++] = y + height;
-        vertices[index++] = z + depth;
-        vertices[index++] = color.toFloatBits();
-
-        // Left top
-        vertices[index++] = x;
-        vertices[index++] = y + height;
-        vertices[index++] = z + depth;
-        vertices[index++] = color.toFloatBits();
-        vertices[index++] = x;
-        vertices[index++] = y + height;
-        vertices[index++] = z;
-        vertices[index++] = color.toFloatBits();
-
-        // Left front
-        vertices[index++] = x;
-        vertices[index++] = y;
-        vertices[index++] = z;
-        vertices[index++] = color.toFloatBits();
-        vertices[index++] = x;
-        vertices[index++] = y + height;
-        vertices[index++] = z;
-        vertices[index++] = color.toFloatBits();
-
-        // Right front
-        vertices[index++] = x + width;
-        vertices[index++] = y;
-        vertices[index++] = z;
-        vertices[index++] = color.toFloatBits();
-        vertices[index++] = x + width;
-        vertices[index++] = y + height;
-        vertices[index++] = z;
-        vertices[index++] = color.toFloatBits();
-
-        // Left back
-        vertices[index++] = x;
-        vertices[index++] = y;
-        vertices[index++] = z + depth;
-        vertices[index++] = color.toFloatBits();
-        vertices[index++] = x;
-        vertices[index++] = y + height;
-        vertices[index++] = z + depth;
-        vertices[index++] = color.toFloatBits();
-
-        // Right back
-        vertices[index++] = x + width;
-        vertices[index++] = y;
-        vertices[index++] = z + depth;
-        vertices[index++] = color.toFloatBits();
-        vertices[index++] = x + width;
-        vertices[index++] = y + height;
-        vertices[index++] = z + depth;
-        vertices[index++] = color.toFloatBits();
     }
 
     public final Color SHAPE_COLOR = Color.GREEN;
