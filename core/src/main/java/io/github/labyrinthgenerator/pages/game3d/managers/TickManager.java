a06e330b80633d9251daf6a969aff991fffd76e8
myusername-dot <andrej.iljuhin@ddgcorp.ru>
2025-07-17 20:51:04 +0300

    log TickManager time

--- core/src/main/java/io/github/labyrinthgenerator/pages/game3d/managers/TickManager.java
+++ core/src/main/java/io/github/labyrinthgenerator/pages/game3d/managers/TickManager.java
@@ -1,6 +1,7 @@
 package io.github.labyrinthgenerator.pages.game3d.managers;
 
 import io.github.labyrinthgenerator.pages.game3d.tickable.Tickable;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -8,6 +9,7 @@ import java.util.Set;
 /**
  * For objects that need to be updated every frame.
  */
+@Slf4j
 public class TickManager {
 
     private final Set<Tickable> entities = new HashSet<>();
@@ -25,10 +27,17 @@ public class TickManager {
     }
 
     public void tickAllEntities(final float delta) {
+        long tickTime = System.nanoTime();
         for (Tickable ent : entities) {
             if (ent.shouldTick()) {
                 ent.tick(delta);
             }
         }
+        logTickDuration(tickTime);
+    }
+
+    private void logTickDuration(long tickTime) {
+        tickTime = System.nanoTime() - tickTime;
+        log.info("TickManager time sec: " + tickTime / 1_000_000_000.0d + ".");
     }
 }
